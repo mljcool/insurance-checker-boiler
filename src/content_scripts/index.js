d622cd3df8392092d376766a72b63786cc0a6ad2
MarkGocela <markloujames.gocela@devpartners.co>
2021-05-29 12:23:26 +0800

    add jquery.min for content script

--- src/content_scripts/index.js
+++ src/content_scripts/index.js
@@ -1,1 +1,10 @@
 console.log('Content scripts has loaded');
+
+$(function() {
+   // developer_mode();
+   console.log('developer_mode');
+});
+
+$(window).bind('hashchange', function() {
+   console.log('hashchange');
+});
