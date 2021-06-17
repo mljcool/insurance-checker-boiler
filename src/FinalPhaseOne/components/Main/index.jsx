2b10293d5b0ec84d4b7f7492888352e747a09de1
MarkGocela <markloujames.gocela@devpartners.co>
2021-06-17 12:25:00 +0800

    add goto settings if zero connections

--- src/FinalPhaseOne/components/Main/index.jsx
+++ src/FinalPhaseOne/components/Main/index.jsx
@@ -1,25 +1,54 @@
-import React, { useContext, Fragment } from 'react';
+import React, { useContext, Fragment, useEffect, useState } from 'react';
 import './style.css';
 import PaperWrapper from 'FinalPhaseOne/components/PaperWrapper';
 import TabFilter from 'FinalPhaseOne/components/TabFilter';
 import Loader from 'FinalPhaseOne/components/Loader';
 import Settings from 'FinalPhaseOne/components/ConnectSettings';
+import GotoSettings from 'FinalPhaseOne/components/GotoSettings';
 import { AppContext } from 'context/AppContext';
 
+const SubMain = () => {
+  const { isToggle, isLoading, insurerListRef } = useContext(AppContext);
+  const [isZeroConnections, setZeroConnections] = useState(true);
+
+  useEffect(() => {
+    console.log('SubMain');
+    const zeroConnections =
+      insurerListRef.filter((insurance) => insurance.isConnected).length === 0;
+    setZeroConnections(zeroConnections);
+  }, []);
+
+  return (
+    !isToggle && (
+      <PaperWrapper isToggle={!isToggle}>
+        {isZeroConnections && <GotoSettings />}
+        {!isZeroConnections && (
+          <Fragment>
+            <Loader isLoading={isLoading} />
+            <TabFilter />
+          </Fragment>
+        )}
+      </PaperWrapper>
+    )
+  );
+};
+
+const SubSettings = ({ isToggle }) => {
+  return (
+    isToggle && (
+      <PaperWrapper isToggle={isToggle}>
+        <Settings />
+      </PaperWrapper>
+    )
+  );
+};
+
 const MainPage = () => {
-  const { isToggle, isLoading, title } = useContext(AppContext);
+  const { isToggle } = useContext(AppContext);
   return (
     <Fragment>
-      {!isToggle && (
-        <PaperWrapper isToggle={!isToggle}>
-          <Loader isLoading={isLoading} />
-          <TabFilter />
-          {title}
-        </PaperWrapper>
-      )}
-      {isToggle && (
-        <PaperWrapper isToggle={isToggle}>{<Settings />}</PaperWrapper>
-      )}
+      <SubMain />
+      <SubSettings isToggle={isToggle} />
     </Fragment>
   );
 };
--- src/popup/Popup.js
+++ src/popup/Popup.js
@@ -4,26 +4,57 @@ import Header from 'FinalPhaseOne/components/Header';
 import MainPage from 'FinalPhaseOne/components/Main';
 import { insurerList } from 'constant/insurers';
 import { AppContext } from '../context/AppContext';
+import { setChromeIdentity } from 'FinalPhaseOne/storage';
+import { getProviderConnections } from 'FinalPhaseOne/services/connect';
 
 const Popup = () => {
   const [isToggle, setIsToggle] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [insurerListRef, setInsurerListRef] = useState(insurerList);
+  const [browserId, setBrowserId] = useState('');
+  /** FUNCTION REFERENCE HERE  **/
 
-  // FUNCTION REFERENCE HERE
   const onToggleSettings = () => {
     setIsToggle((toggle) => !toggle);
   };
   const onRecallConnect = (params) => {};
-  const onUpdateSetListOfConnection = () => {};
+  const onUpdateSetListOfConnection = () => {
+    getAllConnectedProviders(browserId);
+  };
 
-  // LIFE CYCLE HOOKS HERE
+  /** Get ALl connected insurance and set connected status  **/
+  const getAllConnectedProviders = (browserId) => {
+    setIsLoading(true);
+    getProviderConnections(browserId).then(({ succeeded, data }) => {
+      console.log('getProviderConnections', data);
+      if (succeeded) {
+        setInsurerListRef(
+          insurerList.map((insurer) => {
+            insurer.isConnected = data.some(
+              (insurance) => insurance.insurerId === insurer.id
+            );
+            return insurer;
+          })
+        );
+      }
+      setIsLoading(false);
+    });
+  };
 
-  useEffect(() => {}, []);
+  /** LIFE CYCLE  **/
+
+  useEffect(() => {
+    setChromeIdentity((chromeId) => {
+      setBrowserId(chromeId);
+      getAllConnectedProviders(chromeId);
+      console.log('chromeId HERE', chromeId);
+    });
+  }, []);
 
   return (
     <AppContext.Provider
       value={{
+        browserId,
         isLoading,
         isToggle,
         insurerListRef,
