809f62dd11b3c87e703a6c9bc0926d0ad9061b06
MarkGocela <markloujames.gocela@devpartners.co>
2021-10-11 14:33:17 +0800

    change comma to semicolon

--- src/lib/js/util.js
+++ src/lib/js/util.js
@@ -1,48 +1,46 @@
 const getFormattedDate = (date) => {
-   var year = date.getFullYear();
+  var year = date.getFullYear();
 
-   var month = (1 + date.getMonth()).toString();
-   month = month.length > 1 ? month : '0' + month;
+  var month = (1 + date.getMonth()).toString();
+  month = month.length > 1 ? month : '0' + month;
 
-   var day = date.getDate().toString();
-   day = day.length > 1 ? day : '0' + day;
+  var day = date.getDate().toString();
+  day = day.length > 1 ? day : '0' + day;
 
-   return day + '/' + month + '/' + year;
+  return day + '/' + month + '/' + year;
 };
 
 const safeKeys = (data = {}) => {
-   return (keys) => {
-      const exist = (data || {}).hasOwnProperty(keys);
-      return exist ? data[keys] : (data[keys] = null);
-   };
+  return (keys) => {
+    const exist = (data || {}).hasOwnProperty(keys);
+    return exist ? data[keys] : (data[keys] = null);
+  };
 };
 
 const setInitials = (fname = '', lname = '') => {
-   const strsFormat = (str) => (str || '').charAt(0).toUpperCase();
-   const setNames = strsFormat(fname) + '' + strsFormat(lname);
-   return setNames;
+  const strsFormat = (str) => (str || '').charAt(0).toUpperCase();
+  const setNames = strsFormat(fname) + '' + strsFormat(lname);
+  return setNames;
 };
 
-
-// https://mycrm.finance/app/contacts/3165866/summary
 const urlSPliter = () => {
-   return new Promise((resolve, reject) => {
-      const PROD = 5,
-      const SIT = 6,
-      const url = window.location.href;
-      const splitURL = url.split('/');
-      const isInContactPage =
-         splitURL.includes('contacts') && splitURL.length >= 5;
-      if (!!splitURL.length && isInContactPage) {
-         resolve({
-            success: true,
-            familyId: splitURL[PROD],
-         });
-      } else {
-         resolve({
-            success: false,
-            familyId: 0,
-         });
-      }
-   });
+  return new Promise((resolve, reject) => {
+    const PROD = 5;
+    const SIT = 6;
+    const url = window.location.href;
+    const splitURL = url.split('/');
+    const isInContactPage =
+      splitURL.includes('contacts') && splitURL.length >= 5;
+    if (!!splitURL.length && isInContactPage) {
+      resolve({
+        success: true,
+        familyId: splitURL[PROD],
+      });
+    } else {
+      resolve({
+        success: false,
+        familyId: 0,
+      });
+    }
+  });
 };
